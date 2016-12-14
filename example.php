47ce0057da7b90e4500127d6493b64ef493b86f1
eskrano <ilovephp@spaces.ru>
2016-12-14 15:44:13 +0200

    enddd

--- example.php
+++ example.php
@@ -5,8 +5,13 @@ require __DIR__ . '/vendor/autoload.php';
 use Eskrano\Testtask\Storage;
 
 $storage = new Storage(
-    new \Eskrano\Testtask\Converters\XMLConverter()
+    new \Eskrano\Testtask\Converters\JsonConverter()
 );
+
+$storage->setSavePath(__DIR__ . '/converted.txt');
+
+// u can set converter
+//$storage->setConverter(new \Eskrano\Testtask\Converters\XMLConverter());
 
 echo $storage->save([
     'test'  =>  1,
